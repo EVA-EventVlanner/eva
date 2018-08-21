7414a08f931100e327dc68c0f1d989c4f7186f23
dawimuhammad <muhamadhaddawi@gmail.com>
2018-08-21 22:24:28 +0700

    add redux store

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux'
+import reducers from '../reducers'
+
+const store = createStore (
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+)
+
+export default store
