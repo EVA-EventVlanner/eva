8af90ee7d9e1fc727eac82a8b8d526a5239ca0f5
ariefardi <forzaariefpyon@gmail.com>
2018-08-24 22:20:20 +0700

    wanna get item data

--- src/screen/DetailBudgetScreen.js
+++ src/screen/DetailBudgetScreen.js
@@ -1,47 +1,124 @@
 //import liraries
-import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Image } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base'
+import React, { Component } from "react";
+import { ScrollView, StyleSheet, Image } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  Button,
+  Icon,
+  Left,
+  Body,
+  Right
+} from "native-base";
 // create a component
 class DetailBudgetScreen extends Component {
-    render() {
-        if (this.props) {
-            const {navigation} = this.props
-            const eventDetail = navigation.getParam('id')
-            const imageUrl = eventDetail.imageUrl
-            console.log(eventDetail, ' ini event detail')
-            return (
-                <ScrollView>
-                    <Container>
-                        <CardItem style={{height:202}} >
-                        <Image source={{uri: imageUrl}} style={{height: 200, width: null, flex: 1}} />
-                        </CardItem>
-                        <Card>
-                        <CardItem style={{height:80}} >
-                            <Left>
-                                <Thumbnail source={{uri: 'http://pm1.narvii.com/6205/f7213a47a1c1a245f57636614c277539a382c08f_00.jpg'}} />
-                                <Body>
-                                <Text  > {eventDetail.eventName} </Text>
-                                <Text note> {eventDetail.location} </Text>
-                                </Body>
-                            </Left>
-                        </CardItem>
-                        </Card>
-                    </Container>
-                </ScrollView>
-            );   
-        }
+  render() {
+    if (this.props) {
+      const { navigation } = this.props;
+      const eventDetail = navigation.getParam("id");
+      const imageUrl = eventDetail.imageUrl;
+      console.log(eventDetail, " ini event detail");
+      return (
+        <ScrollView>
+          <Container>
+            <CardItem style={{ height: 200 }}>
+              <Image
+                source={{ uri: imageUrl }}
+                style={{ height: 200, width: null, flex: 1 }}
+              />
+            </CardItem>
+            <Card style={{ marginTop: 10, marginLeft: 10, marginRight: 10 }}>
+              <CardItem style={{ height: 80 }}>
+                <Left>
+                  <Body>
+                    <Text> {eventDetail.eventName} </Text>
+                    <Text note> {eventDetail.location} </Text>
+                  </Body>
+                </Left>
+              </CardItem>
+              <CardItem>
+                <Left>
+                  <Button
+                    rounded
+                    large
+                    style={{ width: 200, height: 35, marginLeft: -120 }}
+                  >
+                    <Text style={{ fontSize: 12, marginLeft: 100 }}>
+                      {" "}
+                      POSTED BY{" "}
+                    </Text>
+                  </Button>
+                </Left>
+              </CardItem>
+              <CardItem>
+                <Left>
+                  <Thumbnail
+                    source={{
+                      uri: eventDetail.admin.imageProfile
+                    }}
+                  />
+                  <Body>
+                    <Text>{eventDetail.admin.name}</Text>
+                    <Text>{eventDetail.admin.role[0]}</Text>
+                  </Body>
+                </Left>
+              </CardItem>
+              <CardItem>
+                <Left>
+                  <Button
+                    onPress={() => {
+                      this.setModalVisible(true);
+                    }}
+                    disabled
+                    transparent
+                    textStyle={{ color: "#87838B" }}
+                  >
+                    <Icon name="md-cash" />
+                    <Text style={{ color: "#1c82f0", marginLeft: -9 }}>
+                      Budget : Rp .{" " + eventDetail.budget.toLocaleString()}
+                    </Text>
+                  </Button>
+                </Left>
+                <Right>
+                  <Button
+                    onPress={() => {
+                      this.setModalVisible(true);
+                    }}
+                    disabled
+                    rounded
+                    style={{ width: 120 }}
+                  >
+                    <Icon
+                      style={{ marginLeft: 50, paddingRight: 0 }}
+                      name="md-cart"
+                    />
+                    <Text style={{ marginRight: 30, paddingLeft: 0 }}>
+                      Add Item
+                    </Text>
+                  </Button>
+                </Right>
+              </CardItem>
+            </Card>
+          </Container>
+        </ScrollView>
+      );
     }
+  }
 }
 
 // define your styles
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2c3e50"
+  }
 });
 
 //make this component available to the app
