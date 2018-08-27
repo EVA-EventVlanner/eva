4e601eb10b535c05c5fe0c4c3b50b9782921911a
Rin Ng <Rin08.ng@gmail.com>
2018-08-27 15:06:59 +0700

    Pulled data from db to be displayed

--- src/screen/MyEvents.js
+++ src/screen/MyEvents.js
@@ -1,6 +1,6 @@
 //import liraries
 import React, { Component } from 'react';
-import { StyleSheet, AsyncStorage, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, AsyncStorage, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
 import { Container, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Right, Left, Body } from 'native-base';
 import {connect} from 'react-redux'
 import {fetchingDataUser} from '../actions/eventActions'
@@ -10,86 +10,81 @@ import ModalEvent from '../components/ModalAddEvent'
 class MyEvents extends Component {
     constructor(props){
         super(props)
-        this.state={
-            anotherFake:[
-                {
-                    id:'1',
-                    title: "Joget Joget Di BiFrost Bridge",
-                    image: 'https://vignette.wikia.nocookie.net/marvelmovies/images/1/16/Bifrost.jpg/revision/latest?cb=20110504130352',
-                    status: "member",
-                },
-                {
-                    id:'2',
-                    title: "Joget Joget Di Odin's Vault",
-                    status: "admin",
-                    image: 'https://vignette.wikia.nocookie.net/marvelmovies/images/b/b9/Odin%27s_Vault.png/revision/latest/scale-to-width-down/640?cb=20140216062716',
-                },
-                {
-                    id:"3",
-                    status: "admin",
-                    title: "Joget Joget Di Hall of Asgard",
-                    image: 'https://vignette.wikia.nocookie.net/marvelmovies/images/d/de/HallOfAsgard1-Thor.png/revision/latest/scale-to-width-down/640?cb=20140311045004',
-                }]
-        }
     }
+    
     async componentDidMount () {
         const token = await AsyncStorage.getItem('token')
         console.log('ini token dari myevent', token)
-        this.props.fetchingDataUser()
+        this.props.getUserData()
+        
     }
-    async triggerButton () {
-        // const token = await AsyncStorage.getItem('token')
-        // const userId = await AsyncStorage.getItem('userId')
-        // console.log('ini token', token)
-        // console.log('ini userId', userId)
+
+    goToEvent(item){
+        console.log("ini item: ", item)
+        this.props.navigation.navigate("DetailBudget", {
+            id: item
+        })
     }
+
+    itemCek(item) {
+        console.log('ini yang tesnya dari ', item);
+        
+    }
+
     render() {
-        console.log("ini dari event: " , this.props.events)
-        return (
-            <Container>
-                <Content>
-                    <Card>
-                        <CardItem style={{justifyContent:'center'}}>
-                            <Text style={{textAlign:'center', fontSize:25, fontWeight:'500'}}> Hello, User's Name </Text>
-                        </CardItem>
-                        <CardItem style={{justifyContent:'center'}}>
-                            <Text style={{textAlign:'center', fontSize:20}}> What would you like to do today? </Text>
-                        </CardItem>
-                    </Card> 
-                    <FlatList
-                        data={this.state.anotherFake}
-                        renderItem={({ item, index }) => {
-                            return (
-                            <Card style={{ borderRadius: 10 }}>
-                                <CardItem bordered style={{ borderTopLeftRadius: 8, borderTopRightRadius: 8, borderBottomWidth:0, borderTopWidth:1.5, borderWidth:2,  borderColor: item.status === "admin" ? '#BA55D3': '#009BD2', }}>
-                                    <Left>
-                                        <Text style={{fontWeight:"600"}}>{item.title}</Text>
-                                    </Left>
-                                    <Right>
-                                        <Text style={{textAlign:'right',  color: item.status === "admin" ? '#BA55D3': '#009BD2' }}> <Text style={{fontWeight:"500"}}>Status: </Text>{item.status}</Text>
-                                    </Right>
-                                </CardItem>
-                                <CardItem bordered  style={{ borderBottomLeftRadius: 8, borderBottomRightRadius: 8, borderTopWidth: 1, borderBottomWidth:1.5, borderWidth: 2, borderColor: item.status === "admin" ? '#BA55D3': '#009BD2', }}>
-                                    <Left>
-                                        <Thumbnail large square source={{uri: item.image}} style={{flex:1}}/>
-                                    </Left>
-                                    <Right>
-                                        <Button small rounded style={{padding:0, backgroundColor:'#009BD2'}}>
-                                            <Text style={{color:'white', padding: 0}}> Join </Text>
-                                        </Button>
-                                    </Right>
-                                </CardItem>
-                            </Card>
-                            )
-                        }}
-                        keyExtractor={(item) => item.id}
-                        />
-                    <Right> 
-                        <ModalEvent/>
-                    </Right> 
-                </Content>
-          </Container>
-        );
+        console.log("ini dari event: " , this.props.getUser)
+        if(this.props.getUser.length === 0){
+            return (
+                <ActivityIndicator large style={{justifyContent:'center',flex: 1}} />
+            )
+        }
+        else{
+            let {name, imageProfile, events, role} = this.props.getUser
+            return (
+                <Container>
+                    <Content>
+                        <Card>
+                            <CardItem style={{justifyContent:'center'}}>
+                                <Text style={{textAlign:'center', fontSize:25, fontWeight:'500'}}> Hello, {name} </Text>
+                            </CardItem>
+                            <CardItem style={{justifyContent:'center'}}>
+                                <Text style={{textAlign:'center', fontSize:20}}> What would you like to do today? </Text>
+                            </CardItem>
+                        </Card> 
+                        <FlatList
+                            data={events}
+                            renderItem={({ item, index }) => {
+                                return (
+                                <Card style={{ borderRadius: 10 }} key={String(index)}> 
+                                    <CardItem bordered style={{ borderTopLeftRadius: 8, borderTopRightRadius: 8, borderBottomWidth:0, borderTopWidth:1.5, borderWidth:2,  borderColor: role[index] === "admin" ? '#BA55D3': '#009BD2', }}>
+                                        <Left>
+                                            <Text style={{fontWeight:"600"}}>{item.eventName}</Text>
+                                        </Left>
+                                        <Right>
+                                            <Text style={{textAlign:'right',  color: role[index] === "admin" ? '#BA55D3': '#009BD2' }}> <Text style={{fontWeight:"500"}}>Status: </Text>{role[index]}</Text>
+                                        </Right>
+                                    </CardItem>
+                                    <CardItem bordered  style={{ borderBottomLeftRadius: 8, borderBottomRightRadius: 8, borderTopWidth: 1, borderBottomWidth:1.5, borderWidth: 2, borderColor: role[index] === "admin" ? '#BA55D3': '#009BD2', }}>
+                                        <Left>
+                                            <Thumbnail large square source={{uri: item.imageUrl}} style={{flex:1}}/>
+                                        </Left>
+                                        <Right>
+                                            <Button medium rounded style={{padding:0, backgroundColor:'#009BD2'}} onPress={() => { this.goToEvent(item)}}>
+                                                <Text style={{color:'white', padding: 0}}> View Event </Text>
+                                            </Button>
+                                        </Right>
+                                    </CardItem>
+                                </Card>
+                                )
+                            }}
+                            />
+                        <Right> 
+                            <ModalEvent/>
+                        </Right> 
+                    </Content>
+              </Container>
+            );
+        }
     }
 }
 
@@ -106,12 +101,12 @@ const styles = StyleSheet.create({
 //make this component available to the app
 const mapStateToProps = (state) => {
     return {
-      events : state.eventReducers.events
+      getUser :  state.eventReducers.user
     }
   }
  const mapDispatchToProps = dispatch => {
   return {
-      fetchingDataUser: () => dispatch(fetchingDataUser())
+    getUserData: () => dispatch(fetchingDataUser())
   }
 }
 
