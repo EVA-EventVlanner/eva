6c204d4e0abbc20adf93fb9ee2207a38fccdb2d0
ariefardi <forzaariefpyon@gmail.com>
2018-08-28 18:44:09 +0700

    rubah dikit

--- src/components/ItemToBuy.js
+++ src/components/ItemToBuy.js
@@ -16,7 +16,7 @@ import {
   Right
 } from "native-base";
 
-import ImagePicker from 'react-native-image-picker'
+import ImagePicker from "react-native-image-picker";
 import storageRef from "../firebase/firebase";
 import RNFetchBlob from "react-native-fetch-blob";
 // import RNFetchBlob from "../../node_modules/react-native-fetch-blob/index";
@@ -28,174 +28,174 @@ window.Blob = Blob;
 
 // create a component
 class MyClass extends Component {
-	constructor(props) {
-		super(props)
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	uploadToGoogleStorage () {
-		ImagePicker.showImagePicker({ title: 'Select Image' }, (response) => {
-			
-			console.log('ini response image picker : ', response)
+  uploadToGoogleStorage() {
+    ImagePicker.showImagePicker({ title: "Select Image" }, response => {
+      console.log("ini response image picker : ", response);
 
-			console.log('ini response.uri awal --> ', response.uri)
+      console.log("ini response.uri awal --> ", response.uri);
 
-			response.uri = 'file://' + response.uri
-			response.path = 'file://' + response.path
+      response.uri = "file://" + response.uri;
+      response.path = "file://" + response.path;
 
-			console.log('ini response.uri baru ---> ', response.uri)
+      console.log("ini response.uri baru ---> ", response.uri);
 
-			// format the image data 
-			let image = {
-			  uri: response.uri,
-			  type: response.type,
-			  name: 'myImage' + '-' + Date.now() + '.jpg'
-			}
+      // format the image data
+      let image = {
+        uri: response.uri,
+        type: response.type,
+        name: "myImage" + "-" + Date.now() + ".jpg"
+      };
 
-			// Instantiate a FormData() object
-			let imgBody = new FormData();
-			
-			// append the image to the object with the title 'image'
-			imgBody.append('image', image);
-			let url = `https://eva-server.ariefardi.xyz/vision/analyze`;
+      // Instantiate a FormData() object
+      let imgBody = new FormData();
 
-			console.log('imgBody sent to server --> ', imgBody)
-			
-			// Perform the request. Note the content type - very important
-			fetch(url, {
-				method: 'POST',
-				// headers: {
-				// 	'Accept': 'application/json',
-				// 	'Content-Type': 'multipart/form-data'
-				// },
-				body: imgBody
-			}).then(res => {
-				let visionResult = JSON.parse(res._bodyInit).result
+      // append the image to the object with the title 'image'
+      imgBody.append("image", image);
+      let url = `https://eva-server.ariefardi.xyz/vision/analyze`;
 
-				console.log('ini vision result --> ', visionResult)
-				
-				this.props.navigation.navigate('Result', {
-					visionResult: visionResult
-				})
+      console.log("imgBody sent to server --> ", imgBody);
 
-			}).catch(error => {
-				console.error(error);
-			});
-		});
-	}
+      // Perform the request. Note the content type - very important
+      fetch(url, {
+        method: "POST",
+        // headers: {
+        // 	'Accept': 'application/json',
+        // 	'Content-Type': 'multipart/form-data'
+        // },
+        body: imgBody
+      })
+        .then(res => {
+          let visionResult = JSON.parse(res._bodyInit).result;
 
-	uploadToStorage() {
-		console.log("openGallery");
-		
-		let mime = "application/octet-stream"
+          console.log("ini vision result --> ", visionResult);
 
-		var options = {
-			title: "Select Avatar",
-			customButtons: [{ name: "fb", title: "Choose Photo from Facebook" }],
-			storageOptions: {
-				skipBackup: true,
-				path: "images"
-			}
-		}
+          this.props.navigation.navigate("Result", {
+            visionResult: visionResult
+          });
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    });
+  }
 
- 		ImagePicker.showImagePicker(options, response => {
-			console.log("Response = ", response)
- 			if (response.didCancel) {
-				console.log("User cancelled image picker");
-			} else if (response.error) {
-				console.log("ImagePicker Error: ", response.error);
-			} else if (response.customButton) {
-				console.log("User tapped custom button: ", response.customButton);
-			} else {
- 				// format the image data 
-				const image = {
-					uri: response.uri,
-					data: response.data,
-					type: 'image/jpeg',
-					name: 'myImage' + '-' + Date.now() + '.jpg'
-				}
-				
-				let api_upload_uri = 'https://eva-server.ariefardi.xyz/vision/upload'
-				 
-				const imgBody = new FormData();
-				
-				const uploadUri =
-				Platform.OS === "android" ? response.uri.replace("file://", "") : uri;
-			
-				let uploadBlob = null;
-				
-				fs.readFile(uploadUri, 'base64')
-				.then ( function (data) {
-					console.log('ini data abis reaf file ----> ', data)
-					return Blob.build(data, { type: `${mime}; BASE64`})
-				})
-				.then(function (blob) {
-					console.log('-----> ini blobnya  :', blob)
-				})
+  uploadToStorage() {
+    console.log("openGallery");
 
-				// append the image to the object with the title 'image'
-				imgBody.append('image', image);
+    let mime = "application/octet-stream";
 
-				console.log('ini img body ----> ',imgBody)
+    var options = {
+      title: "Select Avatar",
+      customButtons: [{ name: "fb", title: "Choose Photo from Facebook" }],
+      storageOptions: {
+        skipBackup: true,
+        path: "images"
+      }
+    };
 
- 				axios({
-					url: api_upload_uri,
-					method: 'post',
-					body: imgBody,
-					headers: {
-						'Accept' : 'application/json',
-						'Content-Type' : 'multipart/form-data',
-					}
-				 })
-				.then( function (visionResult) {
-					console.log(visionResult)
-				})
-				.catch( function (error) {
-					console.error(error);
-				})
-			}
-		})
-	}
+    ImagePicker.showImagePicker(options, response => {
+      console.log("Response = ", response);
+      if (response.didCancel) {
+        console.log("User cancelled image picker");
+      } else if (response.error) {
+        console.log("ImagePicker Error: ", response.error);
+      } else if (response.customButton) {
+        console.log("User tapped custom button: ", response.customButton);
+      } else {
+        // format the image data
+        const image = {
+          uri: response.uri,
+          data: response.data,
+          type: "image/jpeg",
+          name: "myImage" + "-" + Date.now() + ".jpg"
+        };
 
-	render() {
-		let item = this.props.item;
-		console.log("=====>>>>", this.props)
-		let {navigate} = this.props.navigation
-		if (item !== undefined) {
-		return (
-			<Card style={{ height: 200, width: 150 }}>
-			<CardItem>
-				<Body
-				style={{
-					justifyContent: "center",
-					alignContent: "center",
-					alignItems: "center"
-				}}
-				>
-				<Thumbnail
-					source={{
-					uri: item.imageItem
-					}}
-				/>
-				</Body>
-			</CardItem>
-			<Body>
-				<Text> {item.itemName} </Text>
-			</Body>
-			<Body style={{ marginTop: -30 }}>
-				<Text> Item Qty: {item.quantity} </Text>
-			</Body>
-			<Body style={{ marginTop: 0 }}>
-				{/* <Button onPress={() => navigate("Result")} rounded> */}
-				<Button onPress={() => this.uploadToGoogleStorage()} rounded>
-				<Icon name="ios-camera" />
-				</Button>
-			</Body>
-			</Card>
-		);
-		} else {
-		return <ActivityIndicator />;
-		}
-	}
+        let api_upload_uri = "https://eva-server.ariefardi.xyz/vision/upload";
+
+        const imgBody = new FormData();
+
+        const uploadUri =
+          Platform.OS === "android" ? response.uri.replace("file://", "") : uri;
+
+        let uploadBlob = null;
+
+        fs.readFile(uploadUri, "base64")
+          .then(function(data) {
+            console.log("ini data abis reaf file ----> ", data);
+            return Blob.build(data, { type: `${mime}; BASE64` });
+          })
+          .then(function(blob) {
+            console.log("-----> ini blobnya  :", blob);
+          });
+
+        // append the image to the object with the title 'image'
+        imgBody.append("image", image);
+
+        console.log("ini img body ----> ", imgBody);
+
+        axios({
+          url: api_upload_uri,
+          method: "post",
+          body: imgBody,
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "multipart/form-data"
+          }
+        })
+          .then(function(visionResult) {
+            console.log(visionResult);
+          })
+          .catch(function(error) {
+            console.error(error);
+          });
+      }
+    });
+  }
+
+  render() {
+    let item = this.props.item;
+    console.log("=====>>>>", this.props);
+    let { navigate } = this.props.navigation;
+    if (item !== undefined) {
+      return (
+        <Card style={{ height: 200, width: 150 }}>
+          <CardItem>
+            <Body
+              style={{
+                justifyContent: "center",
+                alignContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Thumbnail
+                source={{
+                  uri: item.imageItem
+                }}
+              />
+            </Body>
+          </CardItem>
+          <Body>
+            <Text> {item.itemName} </Text>
+          </Body>
+          <Body style={{ marginTop: -30 }}>
+            <Text> Item Qty: {item.quantity} </Text>
+          </Body>
+          <Body style={{ marginTop: 0 }}>
+            {/* <Button onPress={() => navigate("Result")} rounded> */}
+            <Button onPress={() => this.uploadToGoogleStorage()} rounded>
+              <Icon name="ios-camera" />
+            </Button>
+          </Body>
+        </Card>
+      );
+    } else {
+      return <ActivityIndicator />;
+    }
+  }
 }
 
 // define your styles
