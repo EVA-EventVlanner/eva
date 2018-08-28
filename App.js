f35c5a31d7f8f8be73c1b82290d3ec938422057c
Rin Ng <Rin08.ng@gmail.com>
2018-08-28 18:51:42 +0700

    Fixed navigators

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import {Button} from 'native-base';
+import {Button, Icon} from 'native-base';
 import LoginScreen from './src/screen/Login';
 import RegisterScreen from './src/screen/Register'
 import HomeScreen from './src/screen/Home'
@@ -10,42 +10,68 @@ import DetailBudgetScreen from './src/screen/DetailBudgetScreen'
 import ResultScreen from './src/screen/Result'
 import {createStackNavigator, createDrawerNavigator} from 'react-navigation';
 
-const HomeStack = createStackNavigator ({
+// const HomeStack = createStackNavigator ({
+//   Home: {
+//     screen: HomeScreen,
+//     navigationOptions: {
+//       drawerLabel: 'Home',
+//       title : 'Home',
+//       headerTintColor: 'white',
+//     }
+//   },
+//   DetailBudget: {
+//     screen: DetailBudgetScreen,
+//     navigationOptions: {
+//       title: 'Detail',
+//       headerTintColor: 'white'
+//     }
+//   },
+//   Result: {
+//     screen: ResultScreen,
+//     navigationOptions:{
+//       title: 'Result',
+//       headerTintColor: 'white'
+//     }
+//   }
+// },
+// {
+//   navigationOptions: {
+//     headerStyle: {
+//       backgroundColor: "#009BD2"
+//     }
+//   },
+// })
+
+
+const DrawerIcon = (props) =>  {
+
+  return(
+  <View>
+      <TouchableOpacity onPress={() => props.navigation.openDrawer()}>
+          <Icon name="menu" style={{padding: 10, marginLeft:10}} size={20} color="white"/>
+      </TouchableOpacity>
+  </View>
+  );
+};
+
+const GoBack = (props) =>{
+  console.log("Ini propsnya", props)
+  return (
+      <View>
+        <TouchableOpacity onPress={() => props.navigation.goBack()}>
+            <Icon name="arrow-back" style={{padding: 10, marginLeft:10}} size={20} color="white"/>
+        </TouchableOpacity>
+    </View>
+  )
+}
+const DrawerBar = createDrawerNavigator({
   Home: {
     screen: HomeScreen,
-    navigationOptions: {
-      // drawerLabel: 'Home',
-      title : 'Home',
-      headerTintColor: 'white',
-    }
+    title: "Home"
   },
-  DetailBudget: {
-    screen: DetailBudgetScreen,
-    navigationOptions: {
-      title: 'Detail',
-      headerTintColor: 'white'
-    }
-  },
-  Result: {
-    screen: ResultScreen,
-    navigationOptions:{
-      title: 'Result',
-      headerTintColor: 'white'
-    }
-  }
-},
-{
-  navigationOptions: {
-    headerStyle: {
-      backgroundColor: "#009BD2"
-    }
-  },
-})
-const DrawerBar = createDrawerNavigator({
-  Home: HomeStack,
   MyEvents: {
     screen: MyEventScreen,
-    navigationOptions: {
+    navigationOptions:{
       title : 'My Events',
       headerTintColor: 'white',
     }
@@ -61,32 +87,51 @@ const DrawerBar = createDrawerNavigator({
   
 })
 
+
 const StackNav = createStackNavigator({
   Login: { 
     screen: LoginScreen,
     navigationOptions:{
       title: "Login",
       headerTintColor: '#fff',
+      headerLeft: null,
     }
  },
   Register: { 
     screen: RegisterScreen,
-    navigationOptions:{
+    navigationOptions: {
       title: "Register",
       headerTintColor: '#fff',
+      headerLeft: null
     }
   },
-  Home: DrawerBar
-}, 
+  Home: DrawerBar,
+  DetailBudget: {
+    screen: DetailBudgetScreen,
+    navigationOptions: ({navigation})  => ({
+      title: 'Detail',
+      headerTintColor: 'white',
+      headerLeft : <GoBack navigation={navigation} />
+    })
+  },
+  Result: {
+    screen: ResultScreen,
+    navigationOptions: ({navigation})  => ({
+      headerLeft : <GoBack navigation={navigation} />,
+      title: 'Result',
+      headerTintColor: 'white'
+    })
+  }
+},
 {
   initialRouteName: 'Login',
   // headerMode: 'float',
-  navigationOptions: {
+  navigationOptions: ({navigation})  => ({
     headerStyle: {
       backgroundColor: "#009BD2"
     },
-    headerLeft: null
-  },
+    headerLeft : <DrawerIcon navigation={navigation} />,
+  }),
 });
 
 
